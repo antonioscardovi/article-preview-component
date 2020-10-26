9272d6c7cdb08e253c98eb043aeacc8c3a4aed50
Antonio Scardovi <antonio.scardovi@gmail.com>
2020-10-26 22:32:14 +0100

    mobile share save

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function share() {
   var popup = document.getElementById("popup");
   popup.classList.toggle("show");
 }
+
+function mobileShare() {
+  var popup = document.getElementById("mobile-popup");
+  popup.classList.toggle("show");
+}
